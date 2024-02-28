2d6db9b7fbead207e6afa15e8a43f90cac798257
Walter Nwaugo <47696744+walterthesmart@users.noreply.github.com>
2024-02-28 06:28:24 +0100

    initial commit

---
+++ helper-hardhat-config.js
@@ -0,0 +1,42 @@
+const { ethers } = require("hardhat")
+
+const networkConfig = {
+    default: {
+        name: "hardhat",
+        keepersUpdateInterval: "30",
+    },
+    31337: {
+        name: "localhost",
+        subscriptionId: "588",
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei/ not really needed on a mock network
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: ethers.utils.parseEther("0.005"), // 0.01 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+    },
+    11155111: {
+        name: "sepolia",
+        subscriptionId: "9039",
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 150 gwei
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
+        callbackGasLimit: "1500000000", // 150,000 gas
+        vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+    },
+    1: {
+        name: "mainnet",
+        keepersUpdateInterval: "30",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6
+const frontEndContractsFile = "./front-end-alx/constants/contractAddresses.json"
+const frontEndAbiFile = "./front-end-alx/constants/abi.json"
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+    frontEndContractsFile,
+    frontEndAbiFile,
+}
